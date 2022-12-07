279ebea0852bdc8d1d64785246594ad7e739688c
talhane <loubnatalhane3@gmail.com>
2022-12-07 15:40:57 +0100

    Update category-add.php
    KL

--- admin/category-add.php
+++ admin/category-add.php
@@ -3,7 +3,7 @@
 <?php require('layout/topnav.php'); ?>
 <?php require('layout/left-sidebar-short.php'); ?>
 
-
+<?php require('layout/left-sidebar-short.php'); ?>
 <div class="section white-text" style="background: #B35458;">
 
 	<div class="section">
